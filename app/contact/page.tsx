7b298668aaee6623bec27c80dc4d69c9354b06ff
kevin21018212 <mmb2003@iastate.edu>
2024-02-12 17:42:02 -0600

    sorta kinda working contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,33 +1,58 @@
 "use client";
-import Link from "next/link";
+import { motion } from "framer-motion"; // Import motion from Framer Motion
+import { useEffect, useRef } from "react";
 import styles from "./page.module.css";
 
 const ContactPage = () => {
-  return (
-    <div className={styles.contactContainer}>
-      <div className={styles.contactcard}>
-        <div className={styles.title}>
-          <p className="t1">Contact </p>
-          <div className={styles.links}>
-            <Link href="https://www.linkedin.com/in/matthew-bennett-bennett-592102252/">
-              <p>LinkedIn </p>
-            </Link>
+  const words = ["Email", "LinkedIn", "Instagram", "Phone"];
+  const contactCards = [];
 
-            <Link href="mailto:trashlinkmain@gmail.com">
-              <p>Gmail</p>
-            </Link>
-          </div>
-        </div>
-        <div className={styles.infosection}>
-          <div>
-            <p className="t1">Reach Out</p>
-            <p className="m">Feel free to contact me for any inquires</p>
-          </div>
-          <div></div>
-          <div></div>
-        </div>
+  useEffect(() => {
+    let xPercent = 0;
+
+    const animate = () => {
+      if (xPercent > 0) {
+        xPercent = -100;
+      }
+
+      for (let i = 0; i < words.length; i++) {
+        const firstText = document.getElementById(`firstText${i}`);
+        const secondText = document.getElementById(`secondText${i}`);
+
+        if (firstText && secondText) {
+          firstText.style.left = `${firstText.getBoundingClientRect().width}px`;
+          secondText.style.left = `${
+            secondText.getBoundingClientRect().width
+          }px`;
+
+          firstText.style.transform = `translateX(${xPercent}%)`;
+          secondText.style.transform = `translateX(${xPercent}%)`;
+        }
+      }
+
+      requestAnimationFrame(animate);
+      xPercent += 0.1;
+    };
+
+    requestAnimationFrame(animate);
+
+    return () => {};
+  }, []);
+
+  for (let i = 0; i < words.length; i++) {
+    contactCards.push(
+      <div key={i} className={styles.contactCard}>
+        <h1 id={`firstText${i}`} className={styles.text}>
+          {words[i]}
+        </h1>
+        <h1 id={`secondText${i}`} className={styles.text}>
+          {words[i]}
+        </h1>
       </div>
-    </div>
-  );
+    );
+  }
+
+  return <div className={styles.contactContainer}>{contactCards}</div>;
 };
+
 export default ContactPage;
