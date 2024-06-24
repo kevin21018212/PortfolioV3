61713f777e4590a33a804ff0cd87226e85755037
A5NHUN2_deere <BennettMatthew@johnDeere.com>
2024-06-24 13:38:16 -0500

    next link

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,9 +1,10 @@
-"use client";
+'use client';
 
-import styles from "./resume.module.scss";
-import ResumeClasses from "./resumecluster/classes";
-import ResumeWork from "./resumecluster/work";
-import SkillSlider from "./skills/skillslider";
+import Link from 'next/link';
+import styles from './resume.module.scss';
+import ResumeClasses from './resumecluster/classes';
+import ResumeWork from './resumecluster/work';
+import SkillSlider from './skills/skillslider';
 
 const ResumePage = () => {
   return (
@@ -28,22 +29,23 @@ const Header = () => (
     <SubHeading />
   </div>
 );
+const SubHeading = () => {
+  return (
+    <div className={styles.subHeading}>
+      <h4>
+        Below you can see some of my tools or{' '}
+        <Link href='/resume.pdf' download='resume.pdf' className={styles.simpleGradient}>
+          download resume
+        </Link>
+        for a more formal look at my talents
+      </h4>
+    </div>
+  );
+};
 
 const Heading = () => (
   <div className={styles.heading}>
-    <h2>
-      Coding is only a tool of expression. Beauty can only be found in the art
-    </h2>
-  </div>
-);
-
-const SubHeading = () => (
-  <div className={styles.subHeading}>
-    <h4>
-      Below you can see some of my tools or
-      <strong className={styles.simpleGradient}> download resume </strong>
-      for a more formal look at my talents
-    </h4>
+    <h2>Coding is only a tool of expression. Beauty can only be found in the art</h2>
   </div>
 );
 
