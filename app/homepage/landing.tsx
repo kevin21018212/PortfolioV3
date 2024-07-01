031c72fd77e210d71f7fa1e523fa30b013b21f60
kevin21018212 <trashlinkmain@gmail.com>
2024-06-30 21:23:05 -0500

    new landing stuff

--- app/homepage/landing.tsx
+++ app/homepage/landing.tsx
@@ -2,33 +2,53 @@
 
 import { motion } from "framer-motion";
 import styles from "@/styles/homepage/landing.module.scss";
-
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { boxVariants, containerVariants } from "../../utils/framer";
-import Magnetic from "@/utils/animations/magnet";
 
-const Landing = () => {
+import { useState } from "react";
+import useMousePosition from "@/utils/animations/useMousePosition";
+
+const Landing: React.FC = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const { x, y } = useMousePosition();
+  const size = isHovered ? 400 : 50;
+
   return (
     <div className={styles.landingContainer}>
       <div className={styles.liquidGradient}></div>
 
-      <div className={styles.mainBox}>
-        <Magnetic>
-          <motion.div
-            className={styles.textBox}
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-            variants={containerVariants}
+      <div className={styles.textBox}>
+        <motion.div
+          className={styles.mask}
+          animate={{
+            WebkitMaskPosition: `${x != null ? x - size / 2 : 0}px ${
+              y != null ? y - size / 2 : 0
+            }px`,
+            WebkitMaskSize: `${size}px`,
+          }}
+          transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
+        >
+          <h1
+            onMouseEnter={() => {
+              setIsHovered(true);
+            }}
+            onMouseLeave={() => {
+              setIsHovered(false);
+            }}
           >
-            <h1>Developer</h1>
-            <h1>Programmer</h1>
-            <h1>Designer</h1>
-          </motion.div>
-        </Magnetic>
-      </div>
-      {/* Animate infoBox */}
+            Designer
+            <br /> Developer <br />
+            Programmer
+          </h1>
+        </motion.div>
 
+        <div className={styles.body}>
+          <h1>
+            Hi i'm Matthew <br />
+            <span>Hover me</span>
+          </h1>
+        </div>
+      </div>
       <motion.div
         initial="hidden"
         animate="visible"
@@ -36,11 +56,14 @@ const Landing = () => {
         variants={boxVariants}
         className={styles.symbolBox}
       >
-        <FaGithub size={100} color="#F0F0F0" />
+        <a
+          href="https://github.com/kevin21018212"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub size={100} color="#F0F0F0" />
+        </a>
       </motion.div>
-
-      {/* Animate symbolBox */}
-
       <motion.div
         initial="hidden"
         animate="visible"
@@ -48,7 +71,13 @@ const Landing = () => {
         variants={boxVariants}
         className={styles.infoBox}
       >
-        <FaLinkedin size={100} color="#F0F0F0" />
+        <a
+          href="https://www.linkedin.com/in/matthew-bennett-592102252/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin size={100} color="#F0F0F0" />
+        </a>
       </motion.div>
     </div>
   );
