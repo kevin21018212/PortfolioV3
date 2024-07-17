07a5e82579c8aaf20a40068e790c4031478d2efe
A5NHUN2_deere <BennettMatthew@johnDeere.com>
2024-07-17 12:09:49 -0500

    mobile sizing landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import styles from "./page.module.css";
-import Landing from "./homepage/landing";
-import NewProjects from "./homepage/newprojects";
-import NewProjectsMobile from "./homepage/newprojectsmobile";
-import LandingMobile from "./homepage/landingMobile";
+'use client';
+import React, {useState, useEffect} from 'react';
+import styles from './page.module.css';
+import Landing from './homepage/landing';
+import NewProjects from './homepage/newprojects';
+import NewProjectsMobile from './homepage/newprojectsmobile';
+import LandingMobile from './homepage/landingMobile';
 
 export default function Home() {
   const [isMobile, setIsMobile] = useState(false);
@@ -12,8 +12,8 @@ export default function Home() {
   useEffect(() => {
     const handleResize = () => setIsMobile(window.innerWidth <= 1150);
     handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   if (isMobile) {
