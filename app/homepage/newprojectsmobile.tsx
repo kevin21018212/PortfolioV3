51ec042fb5f178ad3a2a3c915c19d5d03b265b7f
A5NHUN2_deere <BennettMatthew@johnDeere.com>
2024-07-17 11:58:10 -0500

    mobile sizing landing

--- app/homepage/newprojectsmobile.tsx
+++ app/homepage/newprojectsmobile.tsx
@@ -1,39 +1,29 @@
-import React from "react";
-import { motion } from "framer-motion";
-import styles from "@/styles/homepage/projectmobile.module.scss";
-import { projectData } from "@/utils/data/projectdata";
-import SpacerContainer from "@/app/global/spacer";
-import ScrambleText from "@/utils/animations/Scramble";
-import Paragraph from "@/utils/animations/Paragraph";
+import React from 'react';
+import {motion} from 'framer-motion';
+import styles from '@/styles/homepage/projectmobile.module.scss';
+import {projectData} from '@/utils/data/projectdata';
+import SpacerContainer from '@/app/global/spacer';
+import ScrambleText from '@/utils/animations/Scramble';
+import Paragraph from '@/utils/animations/Paragraph';
 
 const NewProjectsMobile = () => {
   return (
     <div className={styles.projectsComponent}>
       {projectData.map((project) => (
         <motion.div key={project.id} className={styles.projectsContent}>
-          <div>
-            <SpacerContainer
-              title={project.projectTitle}
-              tag={project.projectTag}
-              handleToggleProject={null}
-              isProjectOpen={false}
-            />
-            <div className={styles.description}>
-              <div style={{ height: "40vh" }}></div>
-              <Paragraph paragraph={project.description} />
-              <div style={{ height: "40vh" }}></div>
+          <SpacerContainer title={project.projectTitle} tag={project.projectTag} handleToggleProject={null} isProjectOpen={false} />
 
-              <div className={styles.info}>
-                <motion.div
-                  whileHover={{ scale: 1.1, borderRadius: "0px" }}
-                  className={styles.image}
-                ></motion.div>
-                <div className={styles.about}>
-                  <h5>
-                    <ScrambleText>{project.projectTech}</ScrambleText>
-                  </h5>
-                </div>
+          <div className={styles.info}>
+            <div className={styles.about}>
+              <motion.div whileHover={{scale: 1.1, borderRadius: '0px'}} className={styles.image}></motion.div>
+              <div className={styles.text}>
+                <h5>
+                  <ScrambleText>{project.projectTech}</ScrambleText>
+                </h5>
               </div>
+            </div>
+            <div className={styles.description}>
+              <Paragraph paragraph={project.description} />
             </div>
           </div>
         </motion.div>
