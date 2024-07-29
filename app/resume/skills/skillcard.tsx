3dd38ab007a0d060ac1164023b11a9dc2a325ace
kevin21018212 <trashlinkmain@gmail.com>
2024-07-28 22:30:33 -0500

    shiny card

--- app/resume/skills/skillcard.tsx
+++ app/resume/skills/skillcard.tsx
@@ -3,26 +3,23 @@ import { useEffect } from "react";
 import styles from "@/styles/resume/skills/skillcard.module.scss";
 import { SkillCardProps } from "@/utils/data/dataType";
 import { getRandomMeshGradient } from "@/utils/functions";
+import { GlareCard } from "@/utils/components/glareCard";
 
 const SkillCard = ({ title, skillName, experience, index }: SkillCardProps) => {
   const cardStyle = {
     backgroundImage: getRandomMeshGradient(),
   };
-
   return (
-    <motion.div
-      className={styles.skillcardContainer}
-      style={cardStyle}
-      whileHover={{ scale: 1.05, rotate: 1 }}
-      whileTap={{ scale: 0.95, rotate: -1 }}
-    >
-      <div className={styles.card}>
-        <div className={styles.text}>
-          <h5>{title}</h5>
-          <h6>{skillName}</h6>
-          <p>{experience}</p>
+    <motion.div className={styles.skillcardContainer}>
+      <GlareCard className={styles.card}>
+        <div className={styles.innerCard} style={cardStyle}>
+          <div className={styles.text}>
+            <h5>{title}</h5>
+            <h6>{skillName}</h6>
+            <p>{experience}</p>
+          </div>
         </div>
-      </div>
+      </GlareCard>
     </motion.div>
   );
 };
